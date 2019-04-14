4f8cdeac039a55daabe6a0281955313f9e65e857
Siva Girish <sivagirish81@gmail.com>
2019-04-14 11:04:20 +0530

    Initial commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\college\pes\computer science\semester wise\semester-4\special topic syllabus sem4\web frameworks\e-commerce\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
